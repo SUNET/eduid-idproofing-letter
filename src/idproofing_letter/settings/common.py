2afc4d63728f4ae5de83fdd336016e56167a7fbe
Johan Lundberg <lundberg@nordu.net>
2015-12-11 18:17:34 +0100

    Moved dev settings from common.py

--- src/idproofing_letter/settings/common.py
+++ src/idproofing_letter/settings/common.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-from os.path import abspath, dirname, join, normpath
+
 
 __author__ = 'lundberg'
 
@@ -11,28 +11,6 @@ http://flask.pocoo.org/docs/0.10/config/#builtin-configuration-values
 """
 
 DEBUG = False
-
-# Absolute filesystem path to the Flask project directory:
-PROJECT_ROOT = dirname(dirname(dirname(dirname(abspath(__file__)))))
-
-# Absolute filesystem path to the secret file which holds this project's
-# SECRET_KEY. Will be auto-generated the first time this file is interpreted.
-SECRET_FILE = normpath(join(PROJECT_ROOT, 'SECRET'))
-
-# Try to load the SECRET_KEY from our SECRET_FILE. If that fails, then generate
-# a random SECRET_KEY and save it into our SECRET_FILE for future loading. If
-# everything fails, then just raise an exception.
-try:
-    SECRET_KEY = open(SECRET_FILE).read().strip()
-except IOError:
-    try:
-        with open(SECRET_FILE, 'w') as f:
-            import random
-            choice = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-            SECRET_KEY = ''.join([random.SystemRandom().choice(choice) for i in range(50)])
-            f.write(SECRET_KEY)
-    except IOError:
-        raise Exception('Cannot open file `%s` for writing.' % SECRET_FILE)
 
 # Database URIs
 IDPROOFING_MONGO_URI = 'mongodb://'
@@ -48,10 +26,13 @@ CELERY_CONFIG = {
     'CELERY_TASK_SERIALIZER': 'json'
 }
 
+# Secret key
+SECRET_KEY = None
+
 # Logging
-LOG_FILE = 'idproofing_letter.log'
+LOG_FILE = None
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOG_LEVEL = 'ERROR'
+LOG_LEVEL = 'INFO'
 LOG_MAX_BYTES = 10000
 LOG_BACKUP_COUNT = 10
 
--- src/idproofing_letter/settings/dev.py
+++ src/idproofing_letter/settings/dev.py
@@ -2,12 +2,33 @@
 
 from __future__ import absolute_import
 
-from os.path import join
-from idproofing_letter.settings.common import PROJECT_ROOT
+from os.path import abspath, dirname, join, normpath
 
 __author__ = 'lundberg'
 
 DEBUG = True
+
+# Absolute filesystem path to the Flask project directory:
+PROJECT_ROOT = dirname(dirname(dirname(dirname(abspath(__file__)))))
+
+# Absolute filesystem path to the secret file which holds this project's
+# SECRET_KEY. Will be auto-generated the first time this file is interpreted.
+SECRET_FILE = normpath(join(PROJECT_ROOT, 'SECRET'))
+
+# Try to load the SECRET_KEY from our SECRET_FILE. If that fails, then generate
+# a random SECRET_KEY and save it into our SECRET_FILE for future loading. If
+# everything fails, then just raise an exception.
+try:
+    SECRET_KEY = open(SECRET_FILE).read().strip()
+except IOError:
+    try:
+        with open(SECRET_FILE, 'w') as f:
+            import random
+            choice = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+            SECRET_KEY = ''.join([random.SystemRandom().choice(choice) for i in range(50)])
+            f.write(SECRET_KEY)
+    except IOError:
+        raise Exception('Cannot open file `%s` for writing.' % SECRET_FILE)
 
 MONGO_URI = 'mongodb://eduid_idproofing_letter:eduid_idproofing_letter_pw@mongodb.docker'
 
